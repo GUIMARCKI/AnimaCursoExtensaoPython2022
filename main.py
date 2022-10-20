314f629069b150783bdea1f8079ed3203343461f
Guilherme Marcki <116230337+GUIMARCKI@users.noreply.github.com>
2022-10-20 00:52:58 +0000

    criar e exibir variáveis

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #Meu primeiro projeto Python
-#
+
 # print() = comando de saida
 print(" hello world!")
 
+# Quando quiser guardar uma String (frase)
+nome = "Guilherme Marcki"
+
+# Quando eu quiser gardar um numero inteiro
+idade = 22
+
+#Exibir meu nome (que esta dentro da varivel nome)
+print(nome)
 
 
-
-
-'''
-Comentario em bloco
-'''
