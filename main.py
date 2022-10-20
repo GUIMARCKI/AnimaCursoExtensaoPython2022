000191efe56a176008eac894f20a326addc0f4ab
Guilherme Marcki <116230337+GUIMARCKI@users.noreply.github.com>
2022-10-20 01:17:39 +0000

    aula 01

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Meu primeiro projeto Python
 
 # print() = comando de saida
-print(" hello world!")
+print(" hello world!\n")
 
 # Quando quiser guardar uma String (frase)
 nome = "Guilherme Marcki"
@@ -12,4 +12,10 @@ idade = 22
 #Exibir meu nome (que esta dentro da varivel nome)
 print(nome)
 
+# Quando quiser exibir a frase 'minha idade é' completando com o conteudo da variavel
+print("\n minha idade é "+ str(idade))
+print(f"minha idade é {idade}")
+print("minha idade é {}".format(idade))
 
+# Quando quiser exibir  as dua vaiaveis
+print("meu nome é {} e tenho {} anos ".format(nome,idade))
